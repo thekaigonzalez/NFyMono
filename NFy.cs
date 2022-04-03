e857272e201a0f3fb898f07d10fe4f408651c509
Kai Gonzalez <gkai70264@gmail.com>
2022-04-03 02:09:11 -0400

    first commit

---
+++ NFy.cs
@@ -0,0 +1,145 @@
+using Godot;
+using System;
+public class NFy : Control
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    public float sp = 0;
+    public float sl = 0;
+    /*
+    Official Naming convention for nodes
+    */
+    
+    public Panel getNFyScreen() {
+        return GetNode<Panel>("NFYSCREEN");    
+    }
+    
+    public AudioStreamPlayer getNFyStream() {
+        return GetNode<AudioStreamPlayer>("NfyStream");
+    }
+    public ProgressBar getNFyBar() {
+        return getNFyScreen().GetNode<ProgressBar>("NFyBar");
+    }
+    
+    public OptionButton getNFySongList() {
+        return getNFyScreen().GetNode<OptionButton>("CurrentSongNF");
+    }
+
+    public bool SpecialsEnabled() {
+        if (OS.GetCmdlineArgs()[1] == "--specials") { return true; } else { return false; }
+    }
+    public void PrintToConsole(string text) {
+            GetNode<TextEdit>("CON").Text += text;
+    }
+
+    public string GetCurrentSongIfAny() {
+        OptionButton sf = getNFySongList();
+        string song = "";
+        song = sf.GetItemText(sf.GetSelectedId());
+        return song;
+    }
+
+    public void OpenSong(string path)
+    {        
+
+        AudioStreamPlayer audSound = getNFyStream();
+
+        File file = new File();
+        Error err = file.Open(path, File.ModeFlags.Read);
+        byte[] b = file.GetBuffer((int)file.GetLen());
+
+
+        if(path.Extension() == "wav")
+        {
+            AudioStreamSample a = new AudioStreamSample();
+            a.Format = AudioStreamSample.FormatEnum.Format16Bits;
+            a.Stereo = true;
+            a.Data = b;
+            sl = a.GetLength();
+            audSound.Stream = a;   
+
+        }
+
+        else if(path.Extension() == "ogg")
+        {
+            AudioStreamOGGVorbis d = new AudioStreamOGGVorbis();
+            d.Data = b;
+            sl = d.GetLength();
+            audSound.Stream = d;   
+        }
+
+        audSound.Play();
+    }
+
+    public void OpenCorrect(string name) {
+        Console.WriteLine(CTEXT("songs/" + name + "."));
+        OpenSong(CTEXT("songs/" + name + ".ogg")); // TODO Implement multiple feature
+    }
+
+    /// Returns the absolute path
+    public string CTEXT(string basel)
+    {
+        return OS.GetExecutablePath().GetBaseDir() + "/" + basel;
+    }
+
+    public bool DirExists(string d) {
+        if (System.IO.Directory.Exists(CTEXT(d))) { return true; } 
+        else { return false; }
+    }
+
+    public string[] listDir(string dname) {
+        return System.IO.Directory.GetFiles(dname);
+    }
+    
+    public string ParseSongEntry(string B) {
+        B = B.Replace("\\", "/"); // Convert to cross platform format
+        return System.IO.Path.GetFileNameWithoutExtension(B);
+    }
+
+    public void SongPreload() {
+
+        if (DirExists("songs")) { // Song preload
+            foreach (string item in listDir("songs")) {
+                if (!item.EndsWith(".import")) { // godot generated 
+                    string item_parsed = ParseSongEntry(item);
+                    getNFySongList().AddItem(item_parsed);
+                }
+            }
+        }
+    }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        SongPreload();
+    }
+    
+    public void _on_Button_pressed() {
+        if (getNFyStream().Stream == null)
+        {
+            OpenCorrect(GetCurrentSongIfAny());
+            getNFyBar().MaxValue = sl;
+        } else {
+            getNFyStream().Play(sp);
+        }
+        
+    }
+
+    public void _onStopRequested() {
+        if (getNFyStream().Stream != null && getNFyStream().Playing) {
+            // try to save position
+            sp = getNFyStream().GetPlaybackPosition();
+            getNFyStream().Stop();
+           
+            
+        }
+    }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if (getNFyStream().Playing) {
+            getNFyBar().Value = getNFyStream().GetPlaybackPosition();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+![Mono NFy](./nfymono.png)
+
+# The NFyMono Project
+
+The NFyMono project provides the LOWEST-LEVEL abstraction to listen to your music as clear as possible.
+
+And it doubles-up the original NFy with **.wav** support!
+
+Written in GDScript and C#! 
