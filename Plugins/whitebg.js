a1c85b3af7930eabf735c24c99fb32e6f7e46b8b
Kai Gonzalez <gkai70264@gmail.com>
2022-04-26 16:13:24 -0400

    New API Example

--- Plugins/whitebg.js
+++ Plugins/whitebg.js
@@ -1,4 +1,6 @@
 function onNMonoEngineStart(e) {
-    NJBackgroundColor("#ffffff");
+    NJBackgroundColor("#31363b");
+    NJFontColor("#d0f0f1")
+    NJButtonUpdate("#2E4053", "#d0f0f1", "#d0f0f1", 2, 5)
 }
 
